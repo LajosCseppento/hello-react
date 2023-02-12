15195031d505ee13d55122b618b30cebb366befd
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-02-12 19:47:20 +0100

    Add Tic Tac Toe (JS/React)

---
+++ 01-tic-tac-toe-tutorial/README.md
@@ -0,0 +1,3 @@
+# hello-react / 01-tic-tac-toe-tutorial
+
+Tic Tac Toe tutorial based on [Intro to React](https://reactjs.org/tutorial/tutorial.html#what-is-react), with a special "wife mode" (see [Stuff Made Here](https://www.youtube.com/StuffMadeHere) for similar features).
---
+++ 01-tic-tac-toe-tutorial/src/index.js
@@ -0,0 +1,159 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+// Missing you Stuff Made Here :-(
+const WIFE_MODE = true;
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{ squares: Array(9).fill(null) }],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+
+    // Simple but does the job (tested)
+    if (WIFE_MODE && this.state.stepNumber > 3 && this.state.xIsNext) {
+      if (i > 0 && squares[i - 1] === null) {
+        console.log("Oops!");
+        i--;
+      }
+    }
+
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([{ squares: squares }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    console.log("Jumping to step", step);
+
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
+  render() {
+    console.log("Game rendering", this.state);
+
+    const history = this.state.history;
+    const squares = history[this.state.stepNumber].squares;
+    const winner = calculateWinner(squares);
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else if (!squares.includes(null)) {
+      status = "Draw";
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    return (
+      <>
+        <h1>Tic Tac Toe (JS/React)</h1>
+        <div className="game">
+          <div className="game-board">
+            <Board squares={squares} onClick={(i) => this.handleClick(i)} />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
