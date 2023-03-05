26245cc3c7ba7b3bd13c9f7d9d23531d45be145b
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-03-05 16:45:12 +0100

    WIP extract top bar

--- 03-react-keycloak/frontend-react/src/App.tsx
+++ 03-react-keycloak/frontend-react/src/App.tsx
@@ -1,44 +1,28 @@
 import EditablePage from './components/EditablePage';
 import ErrorFallback from './components/ErrorFallback';
 import Footer from './components/Footer';
-import NavBar from './components/NavBar';
 import Page from './components/Page';
 import ProfilePage from './components/ProfilePage';
-import Sidebar from './components/Sidebar';
 import SidebarMenu from './components/SidebarMenu';
+import TopBar from './components/TopBar';
 import client from './utils/client';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import ArticleIcon from '@mui/icons-material/Article';
-import EditIcon from '@mui/icons-material/Edit';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import HomeIcon from '@mui/icons-material/Home';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 import MenuIcon from '@mui/icons-material/Menu';
-import RestoreIcon from '@mui/icons-material/Restore';
-import {Container, Divider, Stack} from '@mui/material';
+import {Divider, Stack} from '@mui/material';
 import AppBar from '@mui/material/AppBar';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import Link from '@mui/material/Link';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import React from 'react';
 import {ErrorBoundary} from 'react-error-boundary';
-import {
-  BrowserRouter,
-  Link as RouterLink,
-  Route,
-  Routes,
-  useLocation,
-} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, useLocation} from 'react-router-dom';
 
 const drawerWidth = 240;
 
@@ -75,19 +59,18 @@ function AppRoutes() {
 const App = () => {
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const toggleDrawer =
-    (open?: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event &&
-        event.type === 'keydown' &&
-        ((event as React.KeyboardEvent).key === 'Tab' ||
-          (event as React.KeyboardEvent).key === 'Shift')
-      ) {
-        return;
-      }
+  const toggleDrawer = (open?: boolean) => (event: React.SyntheticEvent) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
 
-      setMobileOpen(typeof open === 'undefined' ? !mobileOpen : open);
-    };
+    setMobileOpen(typeof open === 'undefined' ? !mobileOpen : open);
+  };
 
   const sidebarMenu = <SidebarMenu />;
 
@@ -95,36 +78,7 @@ const App = () => {
     <BrowserRouter>
       <Box sx={{display: 'flex'}}>
         <CssBaseline />
-        <AppBar
-          position="fixed"
-          sx={{zIndex: theme => theme.zIndex.drawer + 1}}
-        >
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              sx={{
-                display: {xs: 'block', sm: 'none'},
-                mr: 2,
-              }}
-              onClick={toggleDrawer()}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" flexGrow="3">
-              React FE Demo
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              component="div"
-              textAlign="right"
-              flexGrow="1"
-            >
-              <em>Fair winds!</em>
-            </Typography>
-          </Toolbar>
-        </AppBar>
+        <TopBar toggleDrawer={toggleDrawer()} />
 
         <Box component="nav" onClick={toggleDrawer(false)}>
           <SwipeableDrawer
