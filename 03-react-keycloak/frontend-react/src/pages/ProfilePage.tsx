a760c306bb3b1bdc3a0a2c1797df933914f288fa
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-03-09 23:38:36 +0100

    Update ProfilePage.tsx

--- 03-react-keycloak/frontend-react/src/pages/ProfilePage.tsx
+++ 03-react-keycloak/frontend-react/src/pages/ProfilePage.tsx
@@ -6,34 +6,38 @@ import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import React, {useContext} from 'react';
 
+import PageTitle from '@app/components/PageTitle';
 import AuthenticationContext from '@app/context/AuthenticationContext';
 
 const ProfilePage = () => {
   const currentUser = useContext(AuthenticationContext).currentUser;
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableBody>
-          <TableRow>
-            <TableCell>Username:</TableCell>
-            <TableCell>{currentUser.username}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>First name:</TableCell>
-            <TableCell>{currentUser.firstName}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Last name:</TableCell>
-            <TableCell>{currentUser.lastName}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>E-mail:</TableCell>
-            <TableCell>{currentUser.email}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <PageTitle value="Profile" />
+      <TableContainer component={Paper}>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>Username:</TableCell>
+              <TableCell>{currentUser.username}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>First name:</TableCell>
+              <TableCell>{currentUser.firstName}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Last name:</TableCell>
+              <TableCell>{currentUser.lastName}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>E-mail:</TableCell>
+              <TableCell>{currentUser.email}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
