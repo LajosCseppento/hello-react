6968b755b8886276d8a63dccc4e031c2ffe928f8
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-03-18 10:22:46 +0100

    Update .prettierrc.js

--- 03-react-keycloak/frontend-react/.prettierrc.js
+++ 03-react-keycloak/frontend-react/.prettierrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   ...require('gts/.prettierrc.json'),
+  plugins: [require('@trivago/prettier-plugin-sort-imports')],
   importOrder: ['^@app/(.*)$', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
